3810f1c9583119aff7bc3bddbf4725d3c6908214
David Kopec <dave@davek-macbook-4.wireless.dartmouth.edu>
2010-10-13 19:31:19 -0400

    Project Part 2 v 1

--- Symtab.h
+++ Symtab.h
@@ -11,12 +11,29 @@
 
 /* Node in a linked list within the symbol table. */
 
+typedef struct NodeAttrs NodeAttrs;
+
+/*we pass around this struct to refer to all of the attributes for a given node*/
+struct NodeAttrs {
+	int size;		/* the size of the data stored in the variable*/
+	char type;		/* a char representing the data type (i = int; d = double; 
+					 p = int *; q = double *; c = char* (for string literals)
+					 f = function; u = undeclared type*/
+	union {
+		int *intAddress;
+		double *doubAddress;
+		char *charAddress;
+	} address;			/*where the data lives in memory*/
+	char *flags;		/*is it a constant, static, etc - note to self - maybe change*/	
+};
+
 typedef struct SymNode SymNode;
 struct SymNode {
-  char *name;	    /* name in this SymNode */
-  int level;	    /* scope level, same for all in this hash table */
-  SymNode *next;    /* next SymNode in list */
-  /* Other attributes go here. */
+	char *name;	    /* name in this SymNode */
+	int level;	    /* scope level, same for all in this hash table */
+	SymNode *next;    /* next SymNode in list */
+	/* Other attributes go here. */
+	NodeAttrs *attrs;
 };
 
 /* Set the name in this node. */
@@ -33,16 +50,16 @@ int GetNodeLevel(SymNode *node);
 
 typedef struct SymHashTable SymHashTable;
 struct SymHashTable {
-  int size;			/* size of hash table */
-  SymNode **table;		/* hash table */
-  SymHashTable *outerScope;	/* ptr to SymHashTable in next outer scope */
-  int level;			/* level of scope, 0 is outermost */
+	int size;			/* size of hash table */
+	SymNode **table;		/* hash table */
+	SymHashTable *outerScope;	/* ptr to SymHashTable in next outer scope */
+	int level;			/* level of scope, 0 is outermost */
 };
 
 /* Symbol table for all levels of scope. */
 
 typedef struct {
-  SymHashTable *innerScope;	/* pointer to SymHashTable of innermost scope */
+	SymHashTable *innerScope;	/* pointer to SymHashTable of innermost scope */
 } SymbolTable;
 
 /* Create an empty symbol table. */
@@ -52,12 +69,12 @@ SymbolTable *CreateSymbolTable();
 void DestroySymbolTable(SymbolTable *symtab);
 
 /* Insert an entry into the innermost scope of symbol table.  First
-   make sure it's not already in that scope.  Return a pointer to the
-   entry. */
+ make sure it's not already in that scope.  Return a pointer to the
+ entry. */
 SymNode *InsertIntoSymbolTable(SymbolTable *symtab, char *name);
 
 /* Lookup an entry in a symbol table.  Return a pointer to it if
-   found, NULL otherwise. */
+ found, NULL otherwise. */
 SymNode *LookupInSymbolTable(SymbolTable *symtab, char *name);
 
 /* Enter a new scope. */
