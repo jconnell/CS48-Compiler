e2dc5f872719fc98fde42a51339b8b04ef183ff5
Jonathan Connell <jconnell@mix.wvu.edu>
2010-11-06 22:57:53 -0400

    Created template files for going from quads to assembly.

---
+++ assemblygen.c
@@ -0,0 +1,11 @@
+/*
+ *  assemblygen.c
+ *  
+ *
+ *  Created by Jonathan Connell on 11/6/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "assemblygen.h"
+
---
+++ assemblygen.h
@@ -0,0 +1,30 @@
+/*
+ *  assemblygen.h
+ *  Assembly Generation from quads.
+ *  Written by Jonathan Connell and David Kopec
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "quads.h"
+#include "ast.h"
+#include "Symtab.h"
+
+/*
+ * Types:
+ *
+ *
+ */
+
+void AssemblyGen(Quad** quads) {
+	while (quads[i]) {
+		switch (quads[i]->op) {
+				case /*ADD*/:
+				case /*SUB*/:
+				case /*DIV*/:
+				case /*MUL*/:
+				default:
+		}
+	}
+}
