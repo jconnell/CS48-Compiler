0e5d1de8d2c65bb0870c30893db1e88dc4a479a2
Jonathan Connell <jconnell@mix.wvu.edu>
2010-11-07 14:00:32 -0500

    Assembly Generation underway. Quads being read and printing to a file.

--- assemblygen.c
+++ assemblygen.c
@@ -8,4 +8,128 @@
  */
 
 #include "assemblygen.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "quads.h"
+#include "ast.h"
+#include "Symtab.h"
+
+/*
+ * Types:
+ *
+ *
+ */
+//GLOBAL VARIABLES
+//int currentQuad = -1;		//index into quads
+//int tempCount = 0;			//for unique temp names
+Quad *quads;				//array of Quads
+//SymbolTable *symtab;		//symbol table
+
+void AssemblyGen(Quad* quads, FILE* file) {//, SymbolTable* symtab) {
+	int i = 0;
+	while (i == 0) {
+		switch (quads->op) {
+			case add:
+				if (quads->addr2.kind == IntConst) {
+					if (quads->addr3.kind == IntConst) {
+						fprintf(file,"%s","Derp");
+					}
+					else if (quads->addr3.kind == DouConst) {
+						continue;
+					}
+				}
+				else if (quads->addr2.kind == DouConst) {
+					if (quads->addr3.kind == IntConst) {
+						continue;
+					}
+					else if (quads->addr3.kind == DouConst) {
+						continue;
+					}
+				}
+			default:
+				printf("DERP");
+				
+				
+				
+				//case /*SUB*/:
+				//case /*DIV*/:
+				//case /*MUL*/:
+				//default:
+		}
+		i++;
+	}
+}
+
+/*int GenQuad(OpKind o, Address a, Address b, Address c)
+{
+	//make the quad
+	Quad *q = malloc(sizeof(Quad));
+	q->op = o;
+	q->addr1 = a;
+	q->addr2 = b;
+	q->addr3 = c;
+	
+	//currentQuad points to the current index into the array of the last
+	//added quad
+	int currentQuad = -1;
+	currentQuad++;
+	
+	//add it to the array
+	quads[currentQuad] = q;
+	
+	return currentQuad;
+}
+
+char *NewTemp()
+{
+	printf("Called NewTemp\n");
+	char *tempName;
+	tempName = malloc(sizeof(char) * 7);
+	sprintf(tempName, "t%d", tempCount);  //$t#
+	
+	printf("Inserting into Symbol Table\n");
+	InsertIntoSymbolTable(symtab, tempName);
+	
+	tempCount++;			//so next one has unique name
+	
+	printf("Leaving NewTemp\n");
+	return tempName;
+}*/
+
+
+int main() {
+	quads = malloc(sizeof(Quad *));
+	
+	FILE *file;
+	file = fopen("tm48code.txt","a+");
+	
+	//char* a1 = malloc(sizeof(char) * 100);
+	//char* a2 = malloc(sizeof(char) * 100);
+	//char* a3 = malloc(sizeof(char) * 100);
+	
+	Address a;
+	Address b;
+	Address c;
+	OpKind o;
+	a.kind = IntConst;
+	b.kind = IntConst;
+	c.kind = IntConst;
+	
+	
+	a.contents.val = 2;
+	b.contents.val = 3;
+	c.contents.val = 4;
+	o = add;
+	
+	quads->op = o;
+	quads->addr1 = a;
+	quads->addr2 = b;
+	quads->addr3 = c;
+	
+	
+	AssemblyGen(quads, file);
+	fclose(file);
+	
+}
 
--- assemblygen.h
+++ assemblygen.h
@@ -4,27 +4,3 @@
  *  Written by Jonathan Connell and David Kopec
  */
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "quads.h"
-#include "ast.h"
-#include "Symtab.h"
-
-/*
- * Types:
- *
- *
- */
-
-void AssemblyGen(Quad** quads) {
-	while (quads[i]) {
-		switch (quads[i]->op) {
-				case /*ADD*/:
-				case /*SUB*/:
-				case /*DIV*/:
-				case /*MUL*/:
-				default:
-		}
-	}
-}
