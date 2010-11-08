b3734cab4b36ba95ba450cbc35f2463df0d39b45
Jonathan Connell <jconnell@mix.wvu.edu>
2010-11-08 14:18:33 -0500

    Small changes to assemblygen.h

--- assemblygen.h
+++ assemblygen.h
@@ -7,7 +7,12 @@
 #include "ast.h"
 #include "quads.h"
 
+#ifndef QUADS_H_
+#define QUADS_H_
+
 typedef enum {math, inequality} AssemKind;
 
 //the main assembly generation function
 void AssemblyGen(Quad** q, FILE* file, SymbolTable* s);
+
+#endif
