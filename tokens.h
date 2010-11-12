e1ac6ee4472c3f09ea3d4c166210ac4a804d1461
Jonathan Connell <jconnell@mix.wvu.edu>
2010-11-11 23:35:07 -0500

    Did initial steps for Project 4 in tokens.h and lexer.l

---
+++ tokens.h
@@ -0,0 +1,44 @@
+/*
+ * tokens.h
+ * 
+ * CS 48 - Project Part 4
+ * Jonathan Connell & David Kopec
+ *
+ */
+
+#define IF			258
+#define ELSE		259
+#define READ		260
+#define	WRITE		261
+#define BANG		262
+#define EQUALS		263
+#define NOTEQUALS	264
+#define GREATER		265
+#define GREATEREQ	266
+#define LESS		267
+#define LESSEQ		268
+#define DO			269
+#define WHILE		270
+#define FOR			271
+#define	LPAREN		272
+#define	RPAREN		273
+#define	LBRACKET	274
+#define	RBRACKET	275
+#define AND			276
+#define OR			277
+#define SEMI		278
+#define	INT			279
+#define DOUBLE		280
+#define CHAR		281
+#define SWITCH		282
+#define CASE		283
+#define CONST		284
+#define	CONTINUE    285
+#define	DEFAULT		286
+#define RETURN		287
+#define SIZEOF		288
+#define STATIC		289
+#define VOID		290
+#define BREAK		291
+#define INCR		292
+#define DECR		293
